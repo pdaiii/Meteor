5c4b949b8608675f5f448a0338ac0c2851487dba
pydai <dai.percy@gmail.com>
2020-04-17 11:35:31 -0700

    Update logger to production environment

--- frontend/store/store.jsx
+++ frontend/store/store.jsx
@@ -6,14 +6,22 @@ import thunk from 'redux-thunk';
 // Handles reducer for entire store
 import rootReducer from '../reducers/root_reducer';
 
+const middlewares = [thunk];
+
+if (process.env.NODE_ENV !== 'production') {
+    middlewares.push(logger);
+}
+
 const configureStore = (preloadedState = {}) => {
     return createStore(
         rootReducer,
         // Uploads preloadedState or persists the current user and session.
         preloadedState,
-        applyMiddleware(thunk, logger)
+        // Use logger only in production environments. Otherwise only use thunk.
+        // applyMiddleware(thunk, logger)
         // applyMiddleware(thunk)
-    )
+        applyMiddleware(...middlewares)
+    );
 };
 
 export default configureStore;
